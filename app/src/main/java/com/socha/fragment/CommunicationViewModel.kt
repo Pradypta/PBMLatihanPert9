a698df17f2bfa2036d3dc92aa4f99a0e5fe75520
socha <dyptapra14@gmail.com>
2019-11-20 16:37:19 +0700

    adding fragment

---
+++ app/src/main/java/com/socha/fragment/CommunicationViewModel.kt
@@ -0,0 +1,10 @@
+package com.socha.fragment
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class CommunicationViewModel: ViewModel(){
+    private val mName = MutableLiveData<String>()
+    val name: LiveData<String> get() = mName
+    fun setName(name: String) { mName.value = name }
+}
